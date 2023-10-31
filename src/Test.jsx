984b52997795779b0f10b2858102c1998db0d3d1
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2023-10-31 17:41:28 +0300

    test map

--- src/Test.jsx
+++ src/Test.jsx
@@ -136,7 +136,7 @@ function Test() {
         </button>
       </div>
       <div class="gmap_canvas">
-        <iframe width="600" height="500" id="gmap_canvas" src={`https://maps.google.com/maps?q=${lat},${long}&t=&z=13&ie=UTF8&iwloc=&output=embed`} frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        <iframe width="600" height="500" id="gmap_canvas" src={`https://maps.google.com/maps?q=${lat},${long}&t=&z=13&ie=UTF8&iwloc=&output=embed`} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" onClick={(e) => console.log(e)}></iframe>
       </div>
     </>
   );
