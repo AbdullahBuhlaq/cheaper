0d720f537bfd79542282070b68a3242a8660f025
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2024-01-13 18:29:03 +0300

    bug fixes

--- src/interfaces/general/data/sponsers.js
+++ src/interfaces/general/data/sponsers.js
@@ -1,3 +1,6 @@
+import img1 from "../../../assets/sponsers/1.jpg";
+import img2 from "../../../assets/sponsers/2.jpg";
+
 export const SPONSERS = ["1- فندق شمينتو - دير عطية", "2- مطعم ضيعتنا - ريف دمشق", "3- اسم تجاري ضخم - موقعه الجغرافي"];
 
-export const IMAGES = ["images/user.webp", "images/user.webp"];
+export const IMAGES = [img1, img2];
